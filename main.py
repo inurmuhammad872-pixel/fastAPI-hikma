b695dd933836a1041fa69b8069ec59928bd5f843
Nurmuhammad <flastdtb@yandex.ru>
2026-05-03 06:45:03 +0500

    add CORS

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from db.database import engine, Base
-
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes.user import router as user_router
 from api.routes.auth import router as auth_router
 
@@ -8,6 +8,17 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://hikma.uz",
+        "http://hikma.uz"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include routers
 app.include_router(user_router)
