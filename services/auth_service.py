288ecad88540cf63cdb3d554a30f6c2f0500cdef
Nurmuhammad <flastdtb@yandex.ru>
2026-05-05 17:36:25 +0500

    fix: auth service password verify bug

--- services/auth_service.py
+++ services/auth_service.py
@@ -20,7 +20,7 @@ def login_user(db: Session, data):
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    if not verify_password(data.password, user.hashed_password):
+    if not verify_password(data.password, str(user.hashed_password)):
         raise HTTPException(status_code=401, detail="Incorrect password")
 
     token = create_access_token({
