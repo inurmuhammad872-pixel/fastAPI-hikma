a4a98149fcddc75fccfa1ba88bd265a2e8f629b0
Nurmuhammad <flastdtb@yandex.ru>
2026-05-06 13:19:32 +0500

    Fix universal phone login

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,7 +1,13 @@
 # services/auth_service.py
 
 from sqlalchemy.orm import Session
-from db.models.user import User
+from db.models.user import (
+    User,
+    TypeAUser,
+    TypeBUser,
+    TypeCUser
+)
+
 from core.security import verify_password
 from core.auth import create_access_token
 from fastapi import HTTPException
@@ -11,18 +17,56 @@ def login_user(db: Session, data):
 
     user = None
 
+    # LOGIN WITH EMAIL
     if data.email:
-        user = db.query(User).filter(User.email == data.email).first()
+        user = db.query(User).filter(
+            User.email == data.email
+        ).first()
 
+    # LOGIN WITH PHONE
     elif data.phone:
-        user = db.query(User).filter(User.phone == data.phone).first()
 
+        profile = (
+            db.query(TypeAUser).filter(
+                TypeAUser.phone == data.phone
+            ).first()
+
+            or
+
+            db.query(TypeBUser).filter(
+                TypeBUser.phone == data.phone
+            ).first()
+
+            or
+
+            db.query(TypeCUser).filter(
+                TypeCUser.phone == data.phone
+            ).first()
+        )
+
+        if profile:
+            user = db.query(User).filter(
+                User.id == profile.id
+            ).first()
+
+    # USER NOT FOUND
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
 
-    if not verify_password(data.password, str(user.hashed_password)):
-        raise HTTPException(status_code=401, detail="Incorrect password")
+    # PASSWORD CHECK
+    if not verify_password(
+        data.password,
+        str(user.hashed_password)
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect password"
+        )
 
+    # CREATE TOKEN
     token = create_access_token({
         "sub": str(user.id),
         "email": user.email,
